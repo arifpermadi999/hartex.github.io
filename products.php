2af9f7ddb1da2213462f6ddf7ad8caeb05faeb05
ARIF PERMADI <arifpermadi999@gmail.com>
2020-10-08 12:29:57 +0700

    init

---
+++ products.php
@@ -0,0 +1,421 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Portfolio - Hartex Bootstrap Template</title>
+  <meta content="" name="descriptison">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+
+  <!-- =======================================================
+  * Template Name: Hartex - v2.1.0
+  * Template URL: https://bootstrapmade.com/Hartex-free-html-bootstrap-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+  <style type="text/css">
+
+/********************* Shopping Demo-6 **********************/
+.product-grid6,.product-grid6 .product-image6{overflow:hidden}
+.product-grid6{font-family:'Open Sans',sans-serif;text-align:center;position:relative;transition:all .5s ease 0s}
+.product-grid6:hover{box-shadow:0 0 10px rgba(0,0,0,.3)}
+.product-grid6 .product-image6 a{display:block}
+.product-grid6 .product-image6 img{width:100%;height:auto;transition:all .5s ease 0s}
+.product-grid6:hover .product-image6 img{transform:scale(1.1)}
+.product-grid6 .product-content{padding:12px 12px 15px;transition:all .5s ease 0s}
+.product-grid6:hover .product-content{opacity:0}
+.product-grid6 .title{font-size:20px;font-weight:600;text-transform:capitalize;margin:0 0 10px;transition:all .3s ease 0s}
+.product-grid6 .title a{color:#000}
+.product-grid6 .title a:hover{color:#2e86de}
+.product-grid6 .price{font-size:18px;font-weight:600;color:#2e86de}
+.product-grid6 .price span{color:#999;font-size:15px;font-weight:400;text-decoration:line-through;margin-left:7px;display:inline-block}
+.product-grid6 .social{background-color:#fff;width:100%;padding:0;margin:0;list-style:none;opacity:0;transform:translateX(-50%);position:absolute;bottom:-50%;left:50%;z-index:1;transition:all .5s ease 0s}
+.product-grid6:hover .social{opacity:1;bottom:20px}
+.product-grid6 .social li{display:inline-block}
+.product-grid6 .social li a{color:#909090;font-size:16px;line-height:45px;text-align:center;height:45px;width:45px;margin:0 7px;border:1px solid #909090;border-radius:50px;display:block;position:relative;transition:all .3s ease-in-out}
+.product-grid6 .social li a:hover{color:#fff;background-color:#2e86de;width:80px}
+.product-grid6 .social li a:after,.product-grid6 .social li a:before{content:attr(data-tip);color:#fff;background-color:#2e86de;font-size:12px;letter-spacing:1px;line-height:20px;padding:1px 5px;border-radius:5px;white-space:nowrap;opacity:0;transform:translateX(-50%);position:absolute;left:50%;top:-30px}
+.product-grid6 .social li a:after{content:'';height:15px;width:15px;border-radius:0;transform:translateX(-50%) rotate(45deg);top:-20px;z-index:-1}
+.product-grid6 .social li a:hover:after,.product-grid6 .social li a:hover:before{opacity:1}
+@media only screen and (max-width:990px){.product-grid6{margin-bottom:30px}
+}
+
+
+
+#category-tabs{
+  width: 100%;
+}     
+#category-tabs a:hover{
+  cursor: pointer;
+}
+#category-tabs nav > .nav.nav-tabs{
+
+  border: none;
+    color:#fff;
+    background:#272e38;
+    border-radius:0;
+
+}
+#category-tabs nav > div a.nav-item.nav-link,
+#category-tabs nav > div a.nav-item.nav-link.active
+{
+  border: none;
+    padding: 18px 25px;
+    color:#fff;
+    background:#272e38;
+    border-radius:0;
+}
+
+#category-tabs .tab-content{
+  background: #fdfdfd;
+    line-height: 25px;
+    border: 1px solid #ddd;
+    border-top:5px solid #e74c3c;
+    border-bottom:5px solid #e74c3c;
+    padding:30px 25px;
+}
+
+#category-tabs nav > div a.nav-item.nav-link:hover,
+#category-tabs nav > div a.nav-item.nav-link:focus
+{
+  border: none;
+    background: #e74c3c;
+    color:#fff;
+    border-radius:0;
+    transition:background 0.20s linear;
+}
+
+.sub-category-tabs{
+  display: none;
+}
+.sub-category-tabs .nav-item{
+    height: 90px;
+  }
+
+.sub-category-tabs nav > .nav.nav-tabs{
+
+  border: none;
+    color:#fff;
+    background:#272e38;
+    border-radius:0;
+
+}
+.sub-category-tabs nav > div a.nav-item.nav-link,
+.sub-category-tabs nav > div a.nav-item.nav-link.active
+{
+  border: none;
+    padding: 18px 25px;
+    color:#fff;
+    background:#272e38;
+    border-radius:0;
+}
+
+.sub-category-tabs nav > div a.nav-item.nav-link.active:after
+ {
+  content: "";
+  position: relative;
+  bottom: -6px;
+  border: 15px solid transparent;
+  border-top-color: #e74c3c ;
+}
+.sub-category-tabs .tab-content{
+  background: #fdfdfd;
+    line-height: 25px;
+    border: 1px solid #ddd;
+    padding:30px 25px;
+}
+
+.sub-category-tabs nav > div a.nav-item.nav-link:hover,
+.sub-category-tabs nav > div a.nav-item.nav-link:focus
+{
+  border: none;
+    background: #e74c3c;
+    color:#fff;
+    border-radius:0;
+    transition:background 0.20s linear;
+}
+  </style>
+</head>
+
+<body>
+<?php 
+    $data = [
+              "nav 1" => ['Diaphragm Pump'=>['1','2'],'Lubrication System' =>['1','2','3','4','5','6','7','8','9','10'],'Safety Device' => ['1','2','3','4'],'Suppression System' =>['1','2','3','4','5','6','7','8']],
+              "nav 2" => ['category','category','category','category'],
+              "nav 3" => ['category','category','category','category'],
+              "nav 4" => ['category','category','category','category'],
+            ]; 
+
+
+?>
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+<!-- 
+      <h1 class="logo mr-auto"><a href="index.html"><span>Com</span>pany</a></h1> -->
+      <!-- Uncomment below if you prefer to use an image logo -->
+       <a href="index.html" class="logo mr-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>
+
+      <nav class="nav-menu d-none d-lg-block">
+        <ul>
+          <li><a href="index.html">Home</a></li>
+
+          <li class="drop-down"><a href="">About</a>
+            <ul>
+              <li><a href="about.html">About Us</a></li>
+              <li><a href="team.html">Team</a></li>
+              <li><a href="testimonials.html">Testimonials</a></li>
+              <li class="drop-down"><a href="#">Deep Drop Down</a>
+                <ul>
+                  <li><a href="#">Deep Drop Down 1</a></li>
+                  <li><a href="#">Deep Drop Down 2</a></li>
+                  <li><a href="#">Deep Drop Down 3</a></li>
+                  <li><a href="#">Deep Drop Down 4</a></li>
+                  <li><a href="#">Deep Drop Down 5</a></li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+
+          <li><a href="services.html">Services</a></li>
+          <li><a href="products.html">Products</a></li>
+          <li><a href="portfolio.html">Portfolio</a></li>
+          <li><a href="pricing.html">Pricing</a></li>
+          <li><a href="blog.html">Blog</a></li>
+          <li><a href="contact.html">Contact</a></li>
+
+        </ul>
+      </nav><!-- .nav-menu -->
+
+      <div class="header-social-links">
+        <a href="#" class="twitter"><i class="icofont-twitter"></i></a>
+        <a href="#" class="facebook"><i class="icofont-facebook"></i></a>
+        <a href="#" class="instagram"><i class="icofont-instagram"></i></a>
+        <a href="#" class="linkedin"><i class="icofont-linkedin"></i></i></a>
+      </div>
+
+    </div>
+  </header><!-- End Header -->
+
+  <main id="main">
+
+    <!-- ======= Breadcrumbs ======= -->
+    <section id="breadcrumbs" class="breadcrumbs">
+      <div class="container">
+
+        <div class="d-flex justify-content-between align-items-center">
+          <h2>Products</h2>
+          <ol>
+            <li><a href="index.html">Home</a></li>
+            <li>Portolio</li>
+          </ol>
+        </div>
+
+      </div>
+    </section><!-- End Breadcrumbs -->
+    <div class="container-fluid" id="category-tabs">
+      <div class="row" data-aos="fade-up">
+        <div style="width: 100%;margin-bottom: 2px;">
+            <nav>
+              <div class="nav nav-tabs nav-fill scrollmenu" id="nav-tab" role="tablist">
+
+                <?php $row = 0;foreach($data as $key => $value): ?>
+                    <a class="nav-item nav-link <?= ($row == 0? 'active':'') ?>" href="#" id="<?= str_replace(" ", "_",$key); ?>"><?= $key ?></a>
+                <?php $row+=1;endforeach ?>
+              </div>
+            </nav>
+        </div>
+      </div>
+    </div>
+    <?php foreach($data as $key => $sub_category): ?>
+      <div class="container-fluid sub-category-tabs" id="<?= str_replace(" ", "_",$key); ?>-section">
+        <div class="row" data-aos="fade-up">
+          <div class="col-xs-12 " style="width: 100%">
+              <nav >
+                <div class="nav nav-tabs nav-fill scrollmenu" id="nav-tab" role="tablist">
+
+                  <?php $row2=0;foreach($sub_category as $key2 => $value2): ?>
+                  <a class="nav-item nav-link <?= ($row2 == 0? 'active':'') ?>" id="<?= str_replace(" ", "_",$key)."-".str_replace(" ", "_",$key2); ?>-tab" data-toggle="tab" href="#<?= str_replace(" ", "_",$key)."-".str_replace(" ", "_",$key2); ?>" role="tab" aria-controls="<?= str_replace(" ", "_",$key2); ?>" aria-selected="true">
+                    <img src="assets/img/sub-category/<?= (is_array($value2)? str_replace(" ", "_",$key2).".png":$value2.'.png')  ?>" style="width: 40px;height: 40px" />
+                    <p><?= $key2 ?></p>
+                  </a>
+                  <?php $row2+=1;endforeach ?>
+                </div>
+              </nav>
+              <div class="tab-content py-3 px-3 px-sm-0" id="nav-tabContent">
+
+                <?php $row2=0;foreach($sub_category as $key2 => $products): ?>
+                  <div class="tab-pane fade show <?= ($row2 == 0? 'active':'') ?>" id="<?= str_replace(" ", "_",$key)."-".str_replace(" ", "_",$key2); ?>" role="tabpanel" aria-labelledby="<?= str_replace(" ", "_",$key2); ?>-tab">
+
+                    <?php if(is_array($products)): ?>
+                    <div class="container">
+                        <h3 class="h3"><?= $key2 ?> </h3>
+                        <div class="row">
+                            <?php foreach($products as $key3 => $val_product): ?>
+                              <div class="col-md-3 col-sm-6">
+                                  <div class="product-grid6">
+                                      <div class="product-image6">
+                                          <a href="#">
+                                              <img class="pic-1" src="assets/img/products/<?= str_replace(" ", "_",$key2); ?>/<?= $val_product ?>.jpg">
+                                          </a>
+                                      </div>
+                                      <div class="product-content">
+                                          <h3 class="title"><a href="#"><?= $key2 ?></a></h3>
+                                          <div class="price">Rp 100.0000
+                                          </div>
+                                      </div>
+                                      <ul class="social">
+                                          <li><a href="" data-tip="Detail"><i class="fa fa-search"></i></a></li>
+                                          
+                                      </ul>
+                                  </div>
+                              </div>
+                            <?php $row2+=1;endforeach ?>  
+                        </div>
+                    </div>
+                    <?php endif ?>
+                  
+                  </div>
+                <?php endforeach ?>
+
+              </div>
+            
+            </div>
+          </div>
+        </div>
+      </div>
+    <?php endforeach ?>
+    
+
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6 footer-contact">
+                    <a href="index.html" class="logo mr-auto"><img src="assets/img/footer-logo.png" alt="" class="img-fluid" style="margin-bottom: 50px" ></a>
+
+            <p>
+              A108 Adam Street <br>
+              New York, NY 535022<br>
+              United States <br><br>
+              <strong>Phone:</strong> +1 5589 55488 55<br>
+              <strong>Email:</strong> info@example.com<br>
+            </p>
+          </div>
+
+          <div class="col-lg-2 col-md-6 footer-links">
+            <h4>Useful Links</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Our Services</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-4 col-md-6 footer-newsletter">
+            <h4>Join Our Newsletter</h4>
+            <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
+            <form action="" method="post">
+              <input type="email" name="email"><input type="submit" value="Subscribe">
+            </form>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container d-md-flex py-4">
+
+      <div class="mr-md-auto text-center text-md-left">
+        <div class="copyright">
+          &copy; Copyright <strong><span>Hartex</span></strong>. All Rights Reserved
+        </div>
+      </div>
+      <div class="social-links text-center text-md-right pt-3 pt-md-0">
+        <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+        <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+        <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+        <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+        <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/jquery-sticky/jquery.sticky.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/venobox/venobox.min.js"></script>
+  <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+  <script type="text/javascript">
+    $(function(){
+      var id_pref = "";
+      $("#category-tabs .nav-item").click(function(){
+        var myid = $(this).attr("id");
+        
+        if(id_pref){
+          $("#" + id_pref + "-section").hide();
+        }
+
+        $("#" + myid + "-section").fadeIn();
+        id_pref = myid;
+
+      })
+    })
+  </script>
+</body>
+
+</html>
